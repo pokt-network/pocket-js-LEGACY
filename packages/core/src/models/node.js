07d078ea30d6685492de2c5396e330a4602a6a2d
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2019-03-14 19:13:37 -0400

    added node isValid and isEqual

--- packages/core/src/models/node.js
+++ packages/core/src/models/node.js
@@ -11,12 +11,28 @@ class Node {
         this.port = port;
         if (ipPort.includes("http://") || ipPort.includes("https://")) {
             this.ipPort = ipPort;
-        }else{
+        } else {
             this.ipPort = requestProtocolStr + ipPort;
-        }  
+        }
     }
 
-    async sendRelay(relay,callback) {
+    isValid() {
+        for (var property in this) {
+            if (!this.hasOwnProperty(property)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    isEqual(netID, network, version) {
+        if (this.netID == netID && this.network == network && this.version == version) {
+            return true;
+        }
+        return false;
+    }
+
+    async sendRelay(relay, callback) {
         try {
             const axiosInstance = axios.create({
                 baseURL: this.ipPort,
@@ -25,28 +41,35 @@ class Node {
                     'Content-Type': 'application/json'
                 }
             });
-            
-            var response = await axiosInstance.post(constants.relayPath, 
+
+            var response = await axiosInstance.post(constants.relayPath,
                 relay.toJSON()
             );
-            
+
             if (response.status == 200 && response.data != null) {
                 var result = response.data;
-        
+
                 if (callback) {
-                  callback(result, null);
+                    callback(result, null);
+                    return;
                 } else {
-                  return result;
+                    return result;
                 }
-              } else {
+            } else {
                 if (callback) {
-                  callback(null, new Error("Failed to send relay with error: " + response.data));
+                    callback(null, new Error("Failed to send relay with error: " + response.data));
+                    return;
                 } else {
-                  return new Error("Failed to send relay with error: " + response.data);
+                    return new Error("Failed to send relay with error: " + response.data);
                 }
-              }
+            }
         } catch (error) {
-            return new Error("Failed to send relay with error: " + error);
+            if (callback) {
+                callback(null, new Error("Failed to send relay with error: " + error));
+                return;
+            } else {
+                return new Error("Failed to send relay with error: " + error);
+            }
         }
     }
 }
