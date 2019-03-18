e75f77e1858ce473e35ac7fe7415a0522edcccaf
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2019-03-18 13:03:24 -0400

    improved getNode flow, retrieveNodes now returns a list of nodes an saves the nodes locally, multiple test fixes

--- packages/core/tests/coreTest.js
+++ packages/core/tests/coreTest.js
@@ -5,6 +5,7 @@ const Package = require("pocket-js-core");
 const Pocket = Package.Pocket;
 
 describe('Pocket Class tests', function () {
+
     it('should instantiate a Pocket instance', function () {
         // Pocket options object
         var opts = {
@@ -47,24 +48,24 @@ describe('Pocket Class tests', function () {
 
         var result = await pocket.retrieveNodes();
 
-        expect(result).to.equal(true);
+        expect(result).to.not.be.an.instanceof(Error);
+        expect(result).to.be.a('array');
     });
 
     it('should fail to retrieve a list of nodes from the Node Dispatcher', async () => {
         // Pocket options object
         var opts = {
             devID: "DEVID1",
-            networkName: "ETH2",
+            networkName: "ETH2",// Wrong network name for intentional error scenario
             netIDs: [40],
             version: "100"
         }
         // New Pocket instance
         var pocket = new Pocket(opts);
-        var pocket = new Pocket(opts);
 
         var result = await pocket.retrieveNodes();
 
-        expect(result).to.equal(false);
+        expect(result).to.be.an.instanceof(Error);
     });
 
     it('should send a relay to a node in the network', async () => {
@@ -82,9 +83,9 @@ describe('Pocket Class tests', function () {
         var address = "0xf892400Dc3C5a5eeBc96070ccd575D6A720F0F9f";
         var data = '{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":["' + address + '","latest"],\"id\":67}';
         // Retrieve nodes first
-        var result = await pocket.retrieveNodes();
-        // Should return true
-        expect(result).to.equal(true);
+        var nodes = await pocket.retrieveNodes();
+        // Should return a list of nodes
+        expect(nodes).to.be.a('array');
         // Create a relay
         var relay = pocket.createRelay("ETH", pocket.configuration.blockchains[0].NetID, version, data, pocket.configuration.devID);
         // Send relay
@@ -109,10 +110,11 @@ describe('Pocket Class tests', function () {
         var address = "0xf892400Dc3C5a5eeBc96070ccd575D6A720F0F9f";
         var data = '{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":["' + address + '","latest"],\"id\":67}';
         // Retrieve nodes first
-        var result = await pocket.retrieveNodes();
-        // Should return true
-        expect(result).to.equal(true);
+        var nodes = await pocket.retrieveNodes();
+        // Should return a list of nodes
+        expect(nodes).to.be.a('array');
         // Create a relay
+        // We are passing a bad netID as second parameter "10" for intentional error scenario
         var relay = pocket.createRelay("ETH", 10, version, data, pocket.configuration.devID);
         // Send relay
         var response = await pocket.sendRelay(relay);
@@ -131,12 +133,14 @@ describe('Pocket Class tests', function () {
         // New Pocket instance
         var pocket = new Pocket(opts);
         // Retrieve nodes first
-        var result = await pocket.retrieveNodes();
+        var nodes = await pocket.retrieveNodes();
+        // Should return a list of nodes
+        expect(nodes).to.be.a('array');
         // Properties for the report class
         var node = pocket.configuration.nodes[0];
+        // TODO: Check if is a Node type object
         expect(node).to.be.an('object');
-        // Should return true
-        expect(result).to.equal(true);
+
         // Create a report
         var report = pocket.createReport(node.ip, "test please ignore");
         // Send report
@@ -157,13 +161,15 @@ describe('Pocket Class tests', function () {
         // New Pocket instance
         var pocket = new Pocket(opts);
         // Retrieve nodes first
-        var result = await pocket.retrieveNodes();
+        var nodes = await pocket.retrieveNodes();
+        // Should return a list of nodes
+        expect(nodes).to.be.a('array');
         // Properties for the report class
         var node = pocket.configuration.nodes[0];
+        // TODO: Check if is a Node type object
         expect(node).to.be.an('object');
-        // Should return true
-        expect(result).to.equal(true);
         // Create a report
+        // Sending empty "ip" parameter to createReport for intentional error scenario.
         var report = pocket.createReport("", "test please ignore");
         // Send report
         var response = await pocket.sendReport(report);
