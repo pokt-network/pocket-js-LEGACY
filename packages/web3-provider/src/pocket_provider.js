0cecb740dda159be4504428d6acec362a19865a6
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2019-03-19 15:00:30 -0400

    Pocket Provider function

--- packages/web3-provider/src/pocket_provider.js
+++ packages/web3-provider/src/pocket_provider.js
@@ -6,3 +6,21 @@
 const Pocket = require("pocket-js-core").Pocket;
 const axios = require('axios');
 
+// Constants
+const TRANSACTION_METHODS = ['eth_sendTransaction', 'eth_sendRawTransaction'];
+
+/**
+ * Pocket Web3 Provider
+ * Sends Relays to a service node in the Pocket Network
+ */
+var PocketProvider = function PocketProvider(networkID, devID, transactionSigner, options) {
+    options = options || {};
+    this.setTransactionSigner(transactionSigner);
+    this.timeout = options.timeout || 10000;
+    this.maxNodes = options.maxNodes || 5;
+    this.connected = false;
+    this.networkID = networkID;
+    this.devID = devID;
+    // Make sure networkID is a string
+    this.networkID = this.networkID.toString();
+};
