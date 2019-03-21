0a523637cdc894be02c8d8e5206f5e374123c04c
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2019-03-21 14:09:59 -0400

    updated pocket provider constructor, added package dependencies, added getNonce, generateRelayData, parseRelayParams and send.

--- packages/web3-provider/src/pocket_provider.js
+++ packages/web3-provider/src/pocket_provider.js
@@ -4,7 +4,7 @@
 
 //  Dependencies
 const Pocket = require("pocket-js-core").Pocket;
-const axios = require('axios');
+var web3Utils = require('web3-utils');
 
 // Constants
 const TRANSACTION_METHODS = ['eth_sendTransaction', 'eth_sendRawTransaction'];
@@ -13,25 +13,181 @@ const TRANSACTION_METHODS = ['eth_sendTransaction', 'eth_sendRawTransaction'];
  * Pocket Web3 Provider
  *  Sends Relays to a service node in the Pocket Network
  * @constructor
- * @param {string} blockchainName - Short Blockchain name (ETH, AION).
- * @param {string} networkID - Subnetwork ID.
- * @param {string} devID - Unique developer ID.
- * @param {object} options - (optional) Object containing the transactionSigner, maxNodes and requestTimeOut.
- * @param {string} options.transactionSigner - (optional) Object containing the TransactionSigner interface methods.
- * @param {number} options.maxNodes - (optional) Maximun amount of nodes to store in instance, default 5.
- * @param {number} options.requestTimeOut - (optional) Maximun timeout for every request in miliseconds, default 10000.
+ * @param {String} networkName - Short Blockchain name (ETH, AION).
+ * @param {String} networkID - Subnetwork ID.
+ * @param {String} devID - Unique developer ID.
+ * @param {String} version - Client version.
+ * @param {Object} options - (optional) Object containing the transactionSigner, maxNodes and requestTimeOut.
+ * @param {String} options.transactionSigner - (optional) Object containing the TransactionSigner interface methods.
+ * @param {Number} options.maxNodes - (optional) Maximun amount of nodes to store in instance, default 5.
+ * @param {Number} options.requestTimeOut - (optional) Maximun timeout for every request in miliseconds, default 10000.
  */
 class PocketProvider {
-    constructor(blockchainName, networkID, devID, options) {
+    constructor(networkName, networkID, devID, version, options) {
         options = options || {};
-        this.blockchainName = blockchainName;
+        this.networkName = networkName;
         this.networkID = networkID;
         this.devID = devID;
+        this.version = version;
         this.transactionSigner = options.transactionSigner;
         this.maxNodes = options.maxNodes || 5;
         this.timeout = options.timeout || 10000;
         this.connected = false;
         // Make sure networkID is a string
         this.networkID = this.networkID.toString();
+        // Pocket Instance
+        this.pocket = new Pocket({
+            devID: this.devID,
+            networkName: this.networkName,
+            netIDs: [this.networkID],
+            version: this.version,
+            maxNodes: this.maxNodes,
+            requestTimeOut: this.timeout
+        })
     }
+
+    /**
+     * Method to get the nonce for a given address
+     * @method _getNonce
+     * @param {String} sender
+     * @param {Function} callback
+     */
+    async _getNonce(sender, callback) {
+        this.send({
+            jsonrpc: '2.0',
+            method: 'eth_getTransactionCount',
+            params: [sender, "latest"],
+            id: (new Date()).getTime()
+        }, function (err, response) {
+            if (err != null) {
+                if (callback) {
+                    callback(err);
+                    return;
+                }
+                return err;
+            } else {
+                var nonce = web3Utils.toDecimal(response.result);
+                if (callback) {
+                    callback(null, nonce);
+                    return;
+                } else {
+                    return nonce;
+                }
+            }
+        });
+    }
+
+    /**
+     * Method to generate the relay data according to the given JSON-RPC payload
+     * @method _generateRelayData
+     * @param {Object} payload
+     * @param {Function} callback
+     */
+    async _generateRelayData(payload, callback) {
+        var method = payload.method;
+
+        if (method === 'eth_sendTransaction') {
+            this._parseRelayParams(payload, callback);
+        } else {
+            var relayData = {
+                "jsonrpc": 2.0,
+                "method": method,
+                "params": payload.params[0],
+                "id": (new Date()).getTime()
+            }
+            if (callback) {
+                callback(null, relayData);
+                return;
+            }
+            return relayData;
+        }
+    }
+
+    /**
+     * @method _parseRelayParams
+     * @param {Object} payload
+     * @param {Function} callback
+     */
+    async _parseRelayParams(payload, callback) {
+        var txParams = payload.params[0];
+        var sender = txParams.from;
+        var _this = this;
+
+        // Verify address exists in the TransactionSigner
+        _this.transactionSigner.hasAddress(sender, function (err, hasAddress) {
+            if (err !== null || hasAddress === false) {
+                if (callback) {
+                    return callback(err);
+                } else {
+                    return err;
+                }
+            }
+
+            // Get the nonce for the sender
+            _this._getNonce(sender, function (err, nonce) {
+                if (err !== null) {
+                    if (callback) {
+                        return callback(err);
+                    } else {
+                        return err;
+                    }
+                }
+                txParams.nonce = web3Utils.toHex(nonce);
+
+                // Signs the transaction with the updated nonce
+                _this.transactionSigner.signTransaction(txParams, function (err, rawTx) {
+                    if (err !== null) {
+                        if (callback) {
+                            return callback(err);
+                        } else {
+                            return err;
+                        }
+                    }
+                    var transactionBody = {
+                        "network": ETH_NETWORK,
+                        "subnetwork": _this.networkId,
+                        "serialized_tx": rawTx,
+                        "tx_metadata": {}
+                    }
+                    if (callback) {
+                        callback(null, transactionBody);
+                        return;
+                    } else {
+                        return transactionBody;
+                    }
+                });
+            });
+        });
+    }
+
+    async send(payload, callback) {
+
+        if (this.pocket instanceof Error) {
+            var error = this.pocket;
+            throw error;
+        }
+
+        var relay = pocket.createRelay(this.networkName, this.networkID, this.version, payload);
+
+        var result = await pocket.sendRelay(relay);
+        if (result instanceof Error) {
+            if (callback) {
+                callback(result);
+                return;
+            } else {
+                return result;
+            }
+        }else{
+            if (callback) {
+                callback(null, result);
+                return;
+            } else {
+                return result;
+            }
+        }
+    };
+}
+
+module.exports = {
+    PocketProvider
 }
--- packages/web3-provider/tests/providerTest.js
+++ packages/web3-provider/tests/providerTest.js
@@ -2,3 +2,64 @@
  * @author Pabel Nunez Landestoy <pabel@pokt.network>
  * @description Unit test for the Pocket Web3 Provider
  */
+var assert = require('assert');
+var PocketProvider = require('../src/pocket_provider.js').PocketProvider;
+var EthereumTx = require('ethereumjs-tx');
+
+var TestOptions = {
+    networkName: "ETH",
+    networkID: "4",
+    devID: "DEVID1",
+    version: "0"
+}
+
+var TestTransactionSigner = {
+    hasAddress: function (address, callback) {
+        console.log(TestTransactionSigner.accounts);
+        callback(null, TestTransactionSigner.accounts.includes(address));
+    },
+    signTransaction: function (txParams, callback) {
+        var pkString = Object.values(TestTransactionSigner.privateKeys)[0];
+        var privateKeyBuffer = Buffer.from(pkString, 'hex');
+        var tx = new EthereumTx(txParams);
+        tx.sign(privateKeyBuffer);
+        callback(null, '0x' + tx.serialize().toString('hex'));
+    },
+    // Needs at least 2 accounts in the node to run all tests
+    accounts: ["0xf892400Dc3C5a5eeBc96070ccd575D6A720F0F9f"],
+    // TODO: Improve this
+    // Update this object with the address - private keys for each account in the same order they are declared
+    privateKeys: {
+        "value": "330D1AD67A9E44E15F5B7EBD20514865CBCE363B2E95FFC9D9C95198EF2893F3"
+    }
+}
+
+describe('PocketProvider', function () {
+    var opts = {
+        transactionSigner: TestTransactionSigner
+    }
+    var provider = new PocketProvider(TestOptions.networkName, TestOptions.networkID, TestOptions.devID, TestOptions.version, opts)
+
+    describe('#send', function () {
+        // Fetch accounts from the service node if the accounts array is empty
+        before(function (done) {
+            if(TestTransactionSigner.accounts.length === 0) {
+                var payload = {
+                    "jsonrpc": "2.0",
+                    "method": "eth_accounts",
+                    "params": [],
+                    "id": 1
+                };
+                provider.send(payload, function (err, result) {
+                    if (err != null) {
+                        throw err;
+                    }
+                    TestTransactionSigner.accounts = result;
+                    done();
+                });
+            }else {
+                done();
+            }
+        });
+    });
+});
