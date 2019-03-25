d154a50115d6b8c15a7422b74b09401ddca52565
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2019-03-25 08:21:56 -0400

    wallet fix

--- packages/core/src/models/wallet.js
+++ packages/core/src/models/wallet.js
@@ -1,6 +1,6 @@
 // Wallet - Model
 class Wallet {
-	constructor(address, privateKey, network, subnetwork, data) {
+	constructor(address, privateKey, network, networkID, data) {
 		this.address = address;
 		this.privateKey = privateKey;
 		this.network = network;
