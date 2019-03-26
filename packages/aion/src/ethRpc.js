e95ff3e5569a041707e6762050c379ad2914931a
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2019-03-26 07:52:06 -0400

    added getTransactionCount

--- packages/aion/src/ethRpc.js
+++ packages/aion/src/ethRpc.js
@@ -49,7 +49,48 @@ class EthRpc {
             }
             return error;
         }
-    }    
+    }  
+
+    async getTransactionCount(address, callback) {
+        try {
+            var data = {
+                "jsonrpc": "2.0",
+                "method": "eth_getTransactionCount",
+                "params": [address, "latest"],
+                "id": (new Date()).getTime()
+            };
+            data = JSON.stringify(data);
+            var relay = this.pocketAion.createRelay(this.networkName, this.netID, data);
+            // Check for errors after creating the relay
+            if (relay instanceof Error) {
+                if (callback) {
+                    callback(relay);
+                }
+                return relay;
+            }
+            var response = await this.pocketAion.sendRelay(relay);
+            // Check for errors after sending the relay
+            if (response instanceof Error) {
+                if (callback) {
+                    callback(response);
+                }
+                return response;
+            }
+            // Parse response
+            var resultJSON = JSON.parse(response).result;
+            var value = this.pocketAion.aionInstance.utils.hexToNumberString(resultJSON)
+
+            if (callback) {
+                callback(null, value);
+            }
+            return value;
+        } catch (error) {
+            if (callback) {
+                callback(error);
+            }
+            return error;
+        }
+    } 
 }
 
 
