87cada710f8f5bd998510ada3fe762b045376db6
Luis de Leon <luyzdeleon@gmail.com>
2019-04-10 23:59:46 -0400

    Updated root package settings

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = Object.assign(
+                                module.exports, 
+                                require('./packages/core'),
+                                require('./packages/eth'),
+                                require('./packages/aion'),
+                                require('./packages/web3-provider'),
+                            );
