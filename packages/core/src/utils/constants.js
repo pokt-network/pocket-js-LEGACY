a25ebc4c3663244b2a8280361404d42f9fa3fca7
Luis de Leon <luyzdeleon@gmail.com>
2019-04-13 23:54:12 -0400

    Updated core

--- packages/core/src/utils/constants.js
+++ packages/core/src/utils/constants.js
@@ -1,5 +1,5 @@
 // Url's and Paths    
-const dispatchNodeURL = "https://dispatch.staging.pokt.network";
+const dispatchNodeURL = "https://dispatch.pokt.network";
 const dispatchPath = "/v1/dispatch";
 const reportPath = "/v1/report";
 const relayPath = "/v1/relay";
