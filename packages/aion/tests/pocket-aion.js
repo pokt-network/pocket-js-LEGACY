f22717f6c7f1be250a7f773b75ee7e843a9761b9
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2019-07-09 12:23:20 -0400

    updated aion and eth test timeouts

--- packages/aion/tests/pocket-aion.js
+++ packages/aion/tests/pocket-aion.js
@@ -15,7 +15,7 @@ const AionContract = PocketJSAion.AionContract;
 const DEV_ID = config.dev_id;
 const MASTERY_NETWORK = 32;
 const MAX_NODES = 10;
-const TIMEOUT = 20000;
+const TIMEOUT = 500000;
 const STORAGE_ADDRESS = "0xa061d41a9de8b2f317073cc331e616276c7fc37a80b0e05a7d0774c9cf956107";
 const BLOCK_HASH = "0xa9316ee7207cf2ac1fd886673d5c14835a86cda97eae8f0d382b95678932c8d0";
 const BLOCK_NUMBER = 1329667;
@@ -52,13 +52,13 @@ describe('Pocket Aion Class tests', function () {
 
         expect(instance).to.not.be.an.instanceof(Error);
         expect(instance).to.be.an.instanceof(PocketAion);
-    });
+    }).timeout(0);
 
     it('should fail to instantiate a Pocket Aion instance', function () {
         expect(function(){
             new PocketAion(null, [MASTERY_NETWORK], MAX_NODES, TIMEOUT);
         }).to.throw(Error);
-    });
+    }).timeout(0);
 
     it('should create a new Aion wallet', function () {
         // New Wallet
@@ -66,7 +66,7 @@ describe('Pocket Aion Class tests', function () {
 
         expect(wallet).to.not.be.an.instanceof(Error);
         expect(wallet).to.be.an.instanceof(Wallet);
-    });
+    }).timeout(0);
 
     it('should import a wallet', function () {
         // Import Wallet
@@ -74,12 +74,12 @@ describe('Pocket Aion Class tests', function () {
 
         expect(importedWallet).to.not.be.an.instanceof(Error);
         expect(importedWallet).to.be.an.instanceof(Wallet);
-    });
+    }).timeout(0);
 
     it('should fail to import a wallet', function () {
         var importedWallet = pocketAion.mastery.importWallet(null);
         expect(importedWallet).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 });
 
 describe('PocketAion ETH Namespace RPC Calls', function () {
@@ -89,189 +89,189 @@ describe('PocketAion ETH Namespace RPC Calls', function () {
 
         expect(balance).to.not.be.an.instanceof(Error);
         expect(balance).to.be.a('string');
-    });
+    }).timeout(0);
 
     it('should fail to retrieve account balance', async () => {
         var balance = await pocketAion.mastery.eth.getBalance(null, "latest");
 
         expect(balance).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should retrieve an account transaction count', async () => {
         var balance = await pocketAion.mastery.eth.getTransactionCount(ADDRESS, "latest");
 
         expect(balance).to.not.be.an.instanceof(Error);
         expect(balance).to.be.a('number');
-    });
+    }).timeout(0);
 
     it('should fail to retrieve an account transaction count', async () => {
         var balance = await pocketAion.mastery.eth.getTransactionCount(null, "latest");
 
         expect(balance).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should retrieve storage information', async () => {
         var balance = await pocketAion.mastery.eth.getStorageAt(STORAGE_ADDRESS, 0, "latest");
 
         expect(balance).to.not.be.an.instanceof(Error);
         expect(balance).to.be.a('string');
-    });
+    }).timeout(0);
 
     it('should fail to retrieve storage information', async () => {
         var balance = await pocketAion.mastery.eth.getStorageAt(null, 0, "latest");
 
         expect(balance).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should retrieve a Block transaction count using the block hash', async () => {
         var txCount = await pocketAion.mastery.eth.getBlockTransactionCountByHash(BLOCK_HASH);
 
         expect(txCount).to.not.be.an.instanceof(Error);
         expect(txCount).to.be.a('number');
-    });
+    }).timeout(0);
 
     it('should fail to retrieve a Block transaction count using the block hash', async () => {
         var txCount = await pocketAion.mastery.eth.getBlockTransactionCountByHash(null);
 
         expect(txCount).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should retrieve a Block transaction count using the block number', async () => {
         var txCount = await pocketAion.mastery.eth.getBlockTransactionCountByNumber(BLOCK_NUMBER);
 
         expect(txCount).to.not.be.an.instanceof(Error);
         expect(txCount).to.be.a('number');
-    });
+    }).timeout(0);
 
     it('should fail to retrieve a Block transaction count using the block number', async () => {
         var txCount = await pocketAion.mastery.eth.getBlockTransactionCountByNumber(null);
 
         expect(txCount).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should retrieve code at the given address', async () => {
         var code = await pocketAion.mastery.eth.getCode(CODE_ADDRESS, "latest");
 
         expect(code).to.not.be.an.instanceof(Error);
         expect(code).to.be.a('string');
-    });
+    }).timeout(0);
 
     it('should fail to retrieve code at the given address', async () => {
         var code = await pocketAion.mastery.eth.getCode(null, null);
 
         expect(code).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should execute a new message call', async () => {
         var code = await pocketAion.mastery.eth.call(null, CALL_TO, 50000, 20000000000, 20000000000, CALL_DATA, "latest");
 
         expect(code).to.not.be.an.instanceof(Error);
         expect(code).to.be.a('string');
-    });
+    }).timeout(0);
 
     it('should fail to execute a new message call', async () => {
         var code = await pocketAion.mastery.eth.call(null, null, 50000, 20000000000, 20000000000, CALL_DATA, "latest");
 
         expect(code).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should retrieve the block information with the block hash', async () => {
         var code = await pocketAion.mastery.eth.getBlockByHash(BLOCK_HASH, true);
 
         expect(code).to.not.be.an.instanceof(Error);
         expect(code).to.be.a('object');
-    });
+    }).timeout(0);
 
     it('should fail to retrieve the block information with the block hash', async () => {
         var code = await pocketAion.mastery.eth.getBlockByHash(null, null);
 
         expect(code).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should retrieve the block information with the block number', async () => {
         var code = await pocketAion.mastery.eth.getBlockByNumber(BLOCK_NUMBER, true);
 
         expect(code).to.not.be.an.instanceof(Error);
         expect(code).to.be.a('object');
-    });
+    }).timeout(0);
 
     it('should fail to retrieve the block information with the block number', async () => {
         var code = await pocketAion.mastery.eth.getBlockByNumber(null, null);
 
         expect(code).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should retrieve a transaction information with the transaction hash', async () => {
         var code = await pocketAion.mastery.eth.getTransactionByHash(TX_HASH);
 
         expect(code).to.not.be.an.instanceof(Error);
         expect(code).to.be.a('object');
-    });
+    }).timeout(0);
 
     it('should fail to retrieve a transaction information with the transaction hash', async () => {
         var code = await pocketAion.mastery.eth.getTransactionByHash(null);
 
         expect(code).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should retrieve a transaction information with the block hash and index', async () => {
         var code = await pocketAion.mastery.eth.getTransactionByBlockHashAndIndex(BLOCK_HASH, 0);
 
         expect(code).to.not.be.an.instanceof(Error);
         expect(code).to.be.a('object');
-    });
+    }).timeout(0);
 
     it('should fail to retrieve a transaction information with the block hash and index', async () => {
         var code = await pocketAion.mastery.eth.getTransactionByBlockHashAndIndex(null, null);
 
         expect(code).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should retrieve a transaction information with the block number and index', async () => {
         var code = await pocketAion.mastery.eth.getTransactionByBlockNumberAndIndex(BLOCK_NUMBER, 0);
 
         expect(code).to.not.be.an.instanceof(Error);
         expect(code).to.be.a('object');
-    });
+    }).timeout(0);
 
     it('should fail to retrieve a transaction information with the block number and index', async () => {
         var code = await pocketAion.mastery.eth.getTransactionByBlockNumberAndIndex(null, null);
 
         expect(code).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should retrieve a transaction receipt using the transaction hash', async () => {
         var code = await pocketAion.mastery.eth.getTransactionReceipt(TX_HASH);
 
         expect(code).to.not.be.an.instanceof(Error);
         expect(code).to.be.a('object');
-    });
+    }).timeout(0);
 
     it('should retrieve a transaction receipt using the transaction hash', async () => {
         var code = await pocketAion.mastery.eth.getTransactionReceipt(null);
 
         expect(code).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should retrieve the logs with the block hash', async () => {
         var code = await pocketAion.mastery.eth.getLogs(null, null, null, null, BLOCK_HASH_LOGS);
 
         expect(code).to.not.be.an.instanceof(Error);
         expect(code).to.be.a('array');
-    });
+    }).timeout(0);
 
     it('should retrieve the estimate gas value for a transaction', async () => {
         var code = await pocketAion.mastery.eth.estimateGas(null, ESTIMATE_GAS_TO, null, null, null, ESTIMATE_GAS_DATA);
 
         expect(code).to.not.be.an.instanceof(Error);
         expect(code).to.be.a('number');
-    });
+    }).timeout(0);
 
     it('should fail to retrieve the estimate gas value for a transaction', async () => {
         var code = await pocketAion.mastery.eth.estimateGas(null, null, null, null, null, ESTIMATE_GAS_DATA);
 
         expect(code).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 
     it('should send a transaction getting the nonce using getTransactionCount', async () => {
         var nonce = await pocketAion.mastery.eth.getTransactionCount(ADDRESS);
@@ -279,13 +279,13 @@ describe('PocketAion ETH Namespace RPC Calls', function () {
 
         expect(result).to.not.be.an.instanceof(Error);
         expect(result).to.be.a('string');
-    });
+    }).timeout(0);
 
     it('should fail to send a transaction without nonce', async () => {
         var result = await pocketAion.mastery.eth.sendTransaction(importedWallet, null, ADDRESS_TO, 50000, 20000000000, 20000000000, null)
 
         expect(result).to.be.an.instanceof(Error);
-    });
+    }).timeout(0);
 });
 describe('PocketAion NET Namespace RPC Calls', function () {
     it('should retrieve the current network id', async () => {
@@ -293,19 +293,19 @@ describe('PocketAion NET Namespace RPC Calls', function () {
 
         expect(result).to.not.be.an.instanceof(Error);
         expect(result).to.be.a('string');
-    });
+    }).timeout(0);
     it('should retrieve the listening status of the node', async () => {
         var result = await pocketAion.mastery.net.listening();
 
         expect(result).to.not.be.an.instanceof(Error);
         expect(result).to.be.a('boolean');
-    });
+    }).timeout(0);
     it('should retrieve the number of peers currently connected', async () => {
         var result = await pocketAion.mastery.net.peerCount();
 
         expect(result).to.not.be.an.instanceof(Error);
         expect(result).to.be.a('number');
-    });
+    }).timeout(0);
 });
 
 describe('PocketAion smart contract interface', function () {
@@ -314,12 +314,12 @@ describe('PocketAion smart contract interface', function () {
 
         expect(result).to.not.be.an.instanceof(Error);
         expect(result).to.be.a('object');
-    });
+    }).timeout(0);
 
     it('should add 1 value to the test smart contract state', async () => {
         var result = await aionContract.executeFunction(FUNCTION_NAME2, importedWallet2, FUNCTION_PARAMS2, null, 50000, 20000000000, 0);
         
         expect(result).to.not.be.an.instanceof(Error);
         expect(result).to.be.a('string');
-    });
+    }).timeout(0);
 });
