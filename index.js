dcfce4dfb84b9022fd4b4971e01d9bea8f7b3a4a
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2019-12-12 23:17:42 -0400

    added tslint and tsconfig, updated index.js

--- index.js
+++ index.js
@@ -2,14 +2,5 @@ module.exports = Object.assign(
                                 module.exports, 
                                 {
                                     PocketJSCore: require('pocket-js-core')
-                                },
-                                {
-                                    PocketJSEth: require('pocket-js-eth')
-                                },
-                                {
-                                    PocketJSAion: require('pocket-js-aion')
-                                },
-                                {
-                                    PocketJSWeb3Provider: require('pocket-js-web3-provider')
                                 }
                             );
