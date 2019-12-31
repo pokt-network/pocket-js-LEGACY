776ee0bc235e38800624c8f95d607c23f25cd1fc
Wilson Garcia <garcia.m.wilson@gmail.com>
2019-12-31 09:58:02 -0400

    Adding new request and response entities
    Close #135

---
+++ src/models/output/BondStatus.ts
@@ -0,0 +1,24 @@
+/**
+ *
+ *
+ * @class BondStatus
+ */
+
+export enum BondStatus {
+  bonded,
+  unbonding,
+  unbonded
+}
+
+export namespace BondStatus {
+  export function getStatus(status: string): BondStatus {
+    switch (status) {
+      case "bonded":
+        return BondStatus.bonded;
+      case "unbonding":
+        return BondStatus.unbonding;
+      default:
+        return BondStatus.unbonded;
+    }
+  }
+}
