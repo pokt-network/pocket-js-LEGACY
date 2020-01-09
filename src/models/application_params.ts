469ffd065da60bbfd379cc6bfe70db3f8afc86f8
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2020-01-09 18:28:42 -0400

    more rebase conflicts

---
+++ src/models/application_params.ts
@@ -0,0 +1,90 @@
+
+/**
+ *
+ *
+ * @class ApplicationParams
+ */
+export class ApplicationParams {
+    /**
+   *
+   * Creates a ApplicationParams object using a JSON string
+   * @param {String} json - JSON string.
+   * @returns {ApplicationParams} - ApplicationParams object.
+   * @memberof ApplicationParams
+   */
+    public static fromJSON(json: string): ApplicationParams {
+        const jsonObject = JSON.parse(json);
+        
+        return new ApplicationParams(
+            jsonObject.unstaking_time,
+            jsonObject.max_applications,
+            jsonObject.app_stake_minimum,
+            jsonObject.baseline_throughput_stake_rate,
+            jsonObject.staking_adjustment,
+            jsonObject.participation_rate_on
+        );
+    }
+
+    public readonly unstakingTime: string;
+    public readonly maxApplications: BigInt;
+    public readonly appStakeMin: BigInt;
+    public readonly baselineThroughputStakeRate: BigInt;
+    public readonly stakingAdjustment: BigInt;
+    public readonly participationRateOn: boolean;
+
+    /**
+     * ApplicationParams.
+     * @constructor
+     * @param {Hex} hash - ApplicationParams hash.
+     * @param {PartSetHeader} parts - Session ApplicationParams Height.
+     */
+    constructor(
+        unstakingTime: string,
+        maxApplications: BigInt,
+        appStakeMin: BigInt,
+        baselineThroughputStakeRate: BigInt,
+        stakingAdjustment: BigInt,
+        participationRateOn: boolean
+    ) {
+        this.unstakingTime = unstakingTime;
+        this.maxApplications = maxApplications;
+        this.appStakeMin = appStakeMin;
+        this.baselineThroughputStakeRate = baselineThroughputStakeRate;
+        this.stakingAdjustment = stakingAdjustment;
+        this.participationRateOn = participationRateOn;
+
+        if (!this.isValid()) {
+            throw new TypeError("Invalid properties length.");
+        }
+    }
+    /**
+   *
+   * Creates a JSON object with the ApplicationParams properties
+   * @returns {JSON} - JSON Object.
+   * @memberof ApplicationParams
+   */
+    public toJSON() {
+        return {
+            "unstaking_time": this.unstakingTime,
+            "max_applications": this.maxApplications,
+            "app_stake_min": this.appStakeMin,
+            "baseline_throughput_stake_rate": this.baselineThroughputStakeRate,
+            "staking_adjustment": this.stakingAdjustment,
+            "participation_rate_on": this.participationRateOn
+        }
+    }
+    /**
+   *
+   * Check if the ApplicationParams object is valid
+   * @returns {boolean} - True or false.
+   * @memberof ApplicationParams
+   */
+    public isValid(): boolean {
+        for (let key in this) {
+            if (!this.hasOwnProperty(key)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/models/stored_proof.ts
@@ -0,0 +1,59 @@
+import { SessionHeader } from "./input/session_header";
+
+/**
+ *
+ *
+ * @class StoredProof
+ */
+export class StoredProof {
+    /**
+   *
+   * Creates a StoredProof object using a JSON string
+   * @param {String} json - JSON string.
+   * @returns {StoredProof} - StoredProof object.
+   * @memberof StoredProof
+   */
+    public static fromJSON(json: string): StoredProof {
+        const jsonObject = JSON.parse(json);
+
+        return new StoredProof(
+            SessionHeader.fromJSON(jsonObject.session_header),
+            jsonObject.servicer_address,
+            jsonObject.total_relays
+        );
+    }
+
+    public readonly sessionHeader: SessionHeader;
+    public readonly servicerAddress: string;
+    public readonly totalRelays: BigInt;
+
+    /**
+     * StoredProof.
+     * @constructor
+     * @param {SessionHeader} sessionHeader - Session Header.
+     * @param {string} servicerAddress - Servicer address.
+     * @param {BigInt} totalRelays - Total amount of relays.
+     */
+    constructor(
+        sessionHeader: SessionHeader,
+        servicerAddress: string,
+        totalRelays: BigInt
+    ) {
+        this.sessionHeader = sessionHeader
+        this.servicerAddress = servicerAddress
+        this.totalRelays = totalRelays
+    }
+    /**
+   *
+   * Creates a JSON object with the StoredProof properties
+   * @returns {JSON} - JSON Object.
+   * @memberof StoredProof
+   */
+    public toJSON() {
+        return {
+            "session_header": this.sessionHeader,
+            "servicer_address": this.servicerAddress,
+            "total_relays": this.totalRelays
+        }
+    }
+}
