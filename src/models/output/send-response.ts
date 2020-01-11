a31fa082b5dbdf6219c29ad7d99bceda96909639
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2020-01-11 13:30:50 -0400

    added sendResponse

---
+++ src/models/output/send-response.ts
@@ -0,0 +1,87 @@
+/**
+ *
+ *
+ * @class SendResponse
+ */
+export class SendResponse {
+    /**
+   *
+   * Creates a SendResponse object using a JSON string
+   * @param {String} json - JSON string.
+   * @returns {SendResponse} - SendResponse object.
+   * @memberof SendResponse
+   */
+    public static fromJSON(json: string): SendResponse {
+        const jsonObject = JSON.parse(json);
+
+        return new SendResponse(
+            jsonObject.data,
+            jsonObject.status,
+            jsonObject.statusText,
+            jsonObject.headers,
+            jsonObject.config
+        );
+    }
+
+    public readonly data: {};
+    public readonly status: number;
+    public readonly statusText: string;
+    public readonly headers: {};
+    public readonly config: {};
+
+    /**
+     * Send Response.
+     * @constructor
+     * @param {Object} data - Reponse body.
+     * @param {number} status - Response status number.
+     * @param {string} statusText - Response status text.
+     * @param {Object} headers - Response headers.
+     * @param {Object} config - Axios configuration used for the request.
+     */
+    constructor(
+        data: {},
+        status: number,
+        statusText: string,
+        headers: {},
+        config: {}
+    ) {
+        this.data = data;
+        this.status = status;
+        this.statusText = statusText;
+        this.headers = headers;
+        this.config = config;
+    
+
+        if (!this.isValid()) {
+            throw new TypeError("Invalid properties length.");
+        }
+    }
+    /**
+   *
+   * Creates a JSON object with the SendResponse properties
+   * @returns {JSON} - JSON Object.
+   * @memberof SendResponse
+   */
+    public toJSON() {
+        return {
+            "data": "data",
+            "status": "status",
+            "statusText": "statusText",
+            "headers": "headers",
+            "config": "config"
+        }
+    }
+    /**
+   *
+   * Check if the SendResponse object is valid
+   * @returns {boolean} - True or false.
+   * @memberof SendResponse
+   */
+    public isValid(): boolean {
+        return this.data != undefined && 
+            this.status != undefined && 
+            this.statusText.length !== 0 &&
+            this.headers != undefined &&
+            this.config != undefined
+    }
+}
