a2354037f48dba9a03ca9009df56dd3413047850
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2020-01-12 16:33:10 -0400

    added getSessionBlockHeight, removed node management from configuration

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -1,4 +1,5 @@
-import { Blockchain, Node } from "../models";
+import { Node } from "../models/node";
+import { PocketAAT } from 'pocket-aat-js';
 
 /**
  *
@@ -6,61 +7,45 @@ import { Blockchain, Node } from "../models";
  * @class Configuration
  */
 export class Configuration {
-  public readonly devID: string;
-  public readonly blockchains: Blockchain[];
-  public readonly maxNodes: number = 5;
-  public readonly requestTimeOut: number = 10000;
-  public readonly sslOnly: boolean = true;
-  public readonly maxSessions: number = 10
-  public nodes: Node[] = [];
 
-  private fs = require("fs")
+  public readonly blockchains: string[]
+  public readonly pocketAAT: PocketAAT
+  public readonly maxNodes: number = 5
+  public readonly requestTimeOut: number = 10000
+  public readonly sslOnly: boolean = true
+  public readonly maxSessions: number = 10
+
   /**
    * Configuration stores settings.
    * @constructor
-   * @param {string} devID - Unique developer ID.
-   * @param {string} blockchains - Blockchain class type list.
+   * @param {string[]} blockchains - Blockchain hash list.
+   * @param {PocketAAT} pocketAAT - Pocket Authentication Token.
    * @param {string} maxNodes - (optional) Maximun amount of nodes to store in instance, default 5.
    * @param {string} requestTimeOut - (optional) Maximun timeout for every request in miliseconds, default 10000.
    * @param {string} sslOnly - (optional) Indicates if you prefer nodes with ssl enabled only, default is true.
+   * @param {Node[]} nodes - (optional) Node list.
    */
   constructor(
-    devID: string,
-    blockchains: Blockchain[],
+    blockchains: string[],
+    pocketAAT: PocketAAT,
     maxNodes: number,
     requestTimeOut: number,
     sslOnly: boolean
   ) {
-    this.devID = devID
     this.blockchains = blockchains
+    this.pocketAAT = pocketAAT,
     this.maxNodes = maxNodes || 5
     this.requestTimeOut = requestTimeOut || 10000
     this.sslOnly = sslOnly || true
   }
-
   /**
-   * Verify if the nodes list is empty
+   * Verify if the configuration is valid
    *
    * @returns {Boolean}
    * @memberof Configuration
    */
-  public nodesIsEmpty() {
-    if (this.nodes === null || this.nodes.length === 0) {
-      return true
-    } else {
-      return false
-    }
-  }
-
-  public setNodes(nodes: Node[]) {
-    this.nodes = nodes
-  }
-
-  public loadNodeList(file: string) {
-    const nodeList = JSON.parse(this.fs.readFileSync(file, "utf8").toString())
-
-    for (const serviceNode of nodeList.serviceNodes) {
-      this.nodes.push(new Node(new Blockchain("", ""), serviceNode))
-    }
+  public isValid(): boolean {
+    this.blockchains.length != 0 && this.pocketAAT.isValid()
+    return false
   }
 }
