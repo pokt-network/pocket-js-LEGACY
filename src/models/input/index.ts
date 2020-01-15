3c6c174a8cff708155a9ef5ca37e56a0dd160381
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2020-01-15 11:42:18 -0400

    updated package.json for the main and types, added proper index.ts files for each directory, allowing a proper index.d.ts file, updated tsconfig

---
+++ src/models/input/index.ts
@@ -0,0 +1,7 @@
+// created from 'create-ts-index'
+
+export * from './dispatch-request'
+export * from './node-proof'
+export * from './relay-payload'
+export * from './relay-request'
+export * from './session-header'
