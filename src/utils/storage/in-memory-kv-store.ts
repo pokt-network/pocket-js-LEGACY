92855228606ce73516c509648d07147962f10e6c
Wilson Garcia <garcia.m.wilson@gmail.com>
2020-01-22 16:10:41 +0100

    Adding comments

--- src/utils/storage/in-memory-kv-store.ts
+++ src/utils/storage/in-memory-kv-store.ts
@@ -1,20 +1,41 @@
 import { IKVStore } from "./kv-store"
 
+
+/**
+ *
+ *
+ * @class InMemoryKVStore
+ * @description Save data using a Key/Value relationship. This object save information in memory, that means if the application is closed,
+ * the saved information will be deleted
+ */
+
 export class InMemoryKVStore implements IKVStore {
   private readonly localStorage: Map<string, any> = new Map()
 
+  /**
+   * @description Associates the specified value with the specified key and save this association in memory.
+   * @param {string} key - key with which the specified value is to be associated
+   * @param {any} value - value to be associated with the specified key
+   */
   public add(key: string, value: any) {
     this.localStorage.set(key, value)
   }
 
-  public get(key: string): any {
+  /**
+   * @description Returns the value to which the specified key is mapped.
+   * @param {string} key - the key whose associated value is to be returned
+   */
+  public get(key: string): string {
     const item = this.localStorage.get(key)
     if (item === undefined) {
       throw new TypeError("Key doesn't exist")
     }
     return item
   }
-
+  
+  /**
+   * @description Returns a list with all the saved value
+   */
   public getItems(): any[] {
     const list = new Array<any>()
 
@@ -28,14 +49,25 @@ export class InMemoryKVStore implements IKVStore {
     return list
   }
 
+  /**
+   * @description Verifies if the object has an association identified with a key.
+   * @param {string} key - key whose mapping is to be checked from the memory
+   */
   public has(key: string): boolean {
     return this.localStorage.has(key)
   }
 
+  /**
+   * @description Removes the mapping for a key from this object if it is present.
+   * @param {string} key - key whose mapping is to be removed from the memory
+   */
   public remove(key: string) {
     this.localStorage.delete(key)
   }
 
+  /**
+   * @description Removes all of the mappings from this object.
+   */
   public clear() {
     this.localStorage.clear()
   }
