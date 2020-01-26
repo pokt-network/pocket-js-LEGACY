1e00777bcda112e3275ef329a5634eb864e85299
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2020-01-26 15:14:54 -0400

    moved nock-utils to the tests folder

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,6 +6,5 @@ export * from './bech-32';
 export * from './enums';
 export * from './hex';
 export * from './key-pair';
-export * from './nock-util';
 export * from './session-manager';
 export * from './type-guard';
