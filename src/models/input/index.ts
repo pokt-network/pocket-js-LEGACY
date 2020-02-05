74af1ad542c2a1611546d9f4868b29806a73d5c1
Luis de Leon <luyzdeleon@gmail.com>
2020-02-05 15:01:55 -0400

    Added auto-generated index files

--- src/models/input/index.ts
+++ src/models/input/index.ts
@@ -2,6 +2,7 @@
 
 export * from './dispatch-request';
 export * from './node-proof';
+export * from './raw-tx-request';
 export * from './relay-payload';
 export * from './relay-request';
 export * from './session-header';
