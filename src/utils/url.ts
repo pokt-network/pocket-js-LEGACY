ea34b581419645f770274fc12107f35f6d599993
Luis de Leon <luyzdeleon@gmail.com>
2020-02-05 14:29:06 -0400

    Added validatePublicKey and validateServiceURL

---
+++ src/utils/url.ts
@@ -0,0 +1,7 @@
+/**
+ * Validates wheter or not a Service URL is valid
+ * @param serviceURL 
+ */
+export function validateServiceURL(serviceURL: URL): boolean {
+    return serviceURL.protocol === "https"
+}
