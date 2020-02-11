0ead7212d19300c6f05735f52675ac4ed4e7861a
Wilson Garcia <garcia.m.wilson@gmail.com>
2020-02-11 13:06:39 -0400

    Supporting infitine number of sessions

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,22 +6,22 @@ import { Node } from "../rpc/models/node"
  */
 export class Configuration {
   public readonly nodes: Node[]
-  public readonly maxNodes: number = 5
-  public readonly maxSessions: number = 100
-  public readonly requestTimeOut: number = 10000
+  public readonly maxNodes: number = 0
+  public readonly maxSessions: number = 0
+  public readonly requestTimeOut: number = 0
   /**
    * Stores multiple properties used to interact with the Pocket Network.
    * @constructor
    * @param {Node[]} nodes - Initial node list.
-   * @param {number} maxNodes - (optional) Maximun amount of nodes to stored in rounting table, default 5.
-   * @param {number} maxSessions - (optional) Maximun amount of sessions to stored for the session manager, default 100.
-   * @param {number} requestTimeOut - (optional) Maximun timeout for every request in miliseconds, default 10000.
+   * @param {number} maxNodes - (optional) Maximun amount of nodes to stored in rounting table, default 0.
+   * @param {number} maxSessions - (optional) Maximun amount of sessions to stored for the session manager, default 0.
+   * @param {number} requestTimeOut - (optional) Maximun timeout for every request in miliseconds, default 0.
    */
   constructor(
     nodes: Node[],
-    maxNodes: number = 5,
-    requestTimeOut: number = 100,
-    maxSessions: number = 10000
+    maxNodes: number = 0,
+    requestTimeOut: number = 0,
+    maxSessions: number = 0
   ) {
     this.nodes = nodes
     this.maxNodes = maxNodes
