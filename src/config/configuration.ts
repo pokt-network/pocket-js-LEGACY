88c30222377643189c94b6c0f297de6da68bc72b
Luis de Leon <luyzdeleon@gmail.com>
2020-04-14 22:25:40 -0400

    Added sessionBlockFrequency blockTime and maxSessionRefreshRetries to Configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -10,6 +10,9 @@ export class Configuration {
   public consensusNodeCount: number = 0
   public readonly requestTimeOut: number = 0
   public readonly acceptDisputedResponses: boolean = false
+  public readonly sessionBlockFrequency: number = 25
+  public readonly blockTime: number = 60000
+  public readonly maxSessionRefreshRetries: number = 3
 
   /**
    * Stores multiple properties used to interact with the Pocket Network.
@@ -19,6 +22,9 @@ export class Configuration {
    * @param {number} consensusNodeCount - (optional) Maximun amount of nodes for local consensus, mandatory ODD number, default 0.
    * @param {number} requestTimeOut - (optional) Maximun timeout for every request in miliseconds, default 0.
    * @param {boolean} acceptDisputedResponses - (optional) Accept or reject responses based on having a full consensus, default false.
+   * @param {number} sessionBlockFrequency - (optional) Amount of blocks that need to elapse for a new session to be tumbled, look at https://github.com/pokt-network/pocket-network-genesis for more information
+   * @param {number} blockTime - (optional) Amount of time (in milliseconds) for a new block to be produced in the Pocket Network
+   * @param {number} maxSessionRefreshRetries - (optional) Amount of times to perform a session refresh in case of getting error code 1124 (Invalid Session)
    * @memberof Configuration
    */
   constructor(
@@ -26,7 +32,10 @@ export class Configuration {
     maxSessions: number = 0,
     consensusNodeCount: number = 0,
     requestTimeOut: number = 0,
-    acceptDisputedResponses: boolean = false
+    acceptDisputedResponses: boolean = false,
+    sessionBlockFrequency: number = 25,
+    blockTime: number = 60000,
+    maxSessionRefreshRetries: number = 3
   ) {
     this.maxDispatchers = maxDispatchers
     this.requestTimeOut = requestTimeOut
@@ -37,6 +46,9 @@ export class Configuration {
       throw new Error("Failed to instantiate a Configuration class object due to consensusNodeCount not being an odd number.")
     }
     this.acceptDisputedResponses = acceptDisputedResponses
+    this.sessionBlockFrequency = sessionBlockFrequency
+    this.blockTime = blockTime
+    this.maxSessionRefreshRetries = maxSessionRefreshRetries
   }
   public setconsensusNodeCount(v: number){
     this.consensusNodeCount = v
