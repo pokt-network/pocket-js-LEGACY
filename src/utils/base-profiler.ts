b8ac22657e16d529fa7c32afd474a70a98edbe53
Alex Firmani <jafirmani@gmail.com>
2021-04-30 15:25:41 -0700

    changing profiler to async

--- src/utils/base-profiler.ts
+++ src/utils/base-profiler.ts
@@ -6,5 +6,5 @@ import { ProfileResult } from "./models/profile-result";
  export abstract class BaseProfiler {
      public data: {key: string, time_elapsed: number | undefined}[] = []
 
-     abstract flushResults(requestID: string, functionName: string, results: ProfileResult[]): void;
+     abstract flushResults(requestID: string, functionName: string, results: ProfileResult[]): Promise<void>;
 }
