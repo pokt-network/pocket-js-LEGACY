8bd1513fd529e7511c74eb1f3abf930a081d89cd
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2021-06-02 17:42:13 -0400

    removed aminojs dependency, removed aminojs implementation, updated tests

--- packages/modules/transactions/src/factory/tx-encoder-factory.ts
+++ packages/modules/transactions/src/factory/tx-encoder-factory.ts
@@ -1,16 +1,10 @@
 import { ProtoTxEncoder } from './proto-tx-encoder'
-import { AminoTxEncoder } from "./amino-tx-encoder"
 import { BaseTxEncoder } from "./base-tx-encoder"
 import { TxMsg } from '../models/msgs'
 import { CoinDenom } from '../models/coin-denom'
 
 export class TxEncoderFactory {
-    public static createEncoder(entropy: string, chainID: string, msg: TxMsg, fee: string, feeDenom?: CoinDenom, memo?: string, useLegacyTxCodec: boolean = false) : BaseTxEncoder {
-        
-        if(useLegacyTxCodec) {
-            return new AminoTxEncoder(entropy, chainID, msg, fee, feeDenom, memo)
-        }
-        
+    public static createEncoder(entropy: string, chainID: string, msg: TxMsg, fee: string, feeDenom?: CoinDenom, memo?: string) : BaseTxEncoder {
         return new ProtoTxEncoder(entropy, chainID, msg, fee, feeDenom, memo)
     }
 }
