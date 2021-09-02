b5e309fe55a8ab22a09f7a88f531c89a801ac59f
Roniel Valdez <roniel_valdez@outlook.com>
2021-09-02 16:19:34 -0400

    fix: remove unused tests due to dispatchers changed  behavior

--- tests/src/unit/routing-table/routing.spec.ts
+++ tests/src/unit/routing-table/routing.spec.ts
@@ -42,15 +42,6 @@ describe('Routing Table tests',() => {
             expect(readDispatcher).to.be.an.instanceof(URL)
         }).timeout(0)
     
-        it('should be able to delete a node from the routing table', () => {
-            const configuration = getConfig()
-    
-            const routing = new RoutingTable([dispatcher], configuration, store)
-            routing.deleteDispatcher(dispatcher)
-            
-            expect(routing.getDispatcher(dispatcher)).to.be.an.instanceof(Error)
-        }).timeout(0)
-    
         it('it should not allow to add more than the maxDispatchers configuration variable number of entries into the routing table.', () => {
             const configuration = getConfig()
     
@@ -129,41 +120,6 @@ describe('Routing Table tests',() => {
             
             expect(dispatcherAdded).to.be.false
         }).timeout(0)
-    
-        it('should fail to delete a node from the routing table since it doesnt exist', () => {
-            const badDispatcher = new URL("http://baddispatcher:8081")
-            const configuration = getConfig()
-    
-            const routing = new RoutingTable([dispatcher], configuration, store)
-            const dispatcherDeleted = routing.deleteDispatcher(badDispatcher)
-            
-            expect(dispatcherDeleted).to.be.false
-        }).timeout(0)
-    
-        it('should fail to read a random node due to empty routing table', () => {
-            const configuration = getConfig()
-            
-            const routing = new RoutingTable([dispatcher], configuration, store)
-            const dispatcherDeleted = routing.deleteDispatcher(dispatcher)
-            expect(dispatcherDeleted).to.be.true
-            
-            const randomDispatcherOrError = routing.getRandomDispatcher()
-            expect(randomDispatcherOrError).to.be.an.instanceof(Error)
-        }).timeout(0)
-    
-        it('should fail to read multiple random nodes from the routing table fue to empty dispatchers list', () => { // Test doesn't currently check randomness of results
-            const configuration = getConfig()
-            
-            const routing = new RoutingTable([dispatcher], configuration, store)
-    
-            const dispatcherDeleted = routing.deleteDispatcher(dispatcher)
-            expect(dispatcherDeleted).to.be.true
-            
-            const randomDispatchersOrError = routing.getRandomDispatchers(3)
-            expect(randomDispatchersOrError).to.be.an.instanceof(Error)
-        }).timeout(0)
     })
-
-    
 
 }).timeout(0)
