f395814dda94b57bcc492d4db305a602f098538f
CrisOG <40803711+crisog@users.noreply.github.com>
2022-04-26 22:58:28 -0400

    feat: core 0.8 tx rpc upgrade (#451)
    * feat: core 0.8 tx rpc changes
    
    * chore: update version

--- src/rpc/models/output/query-block-txs-response.ts
+++ src/rpc/models/output/query-block-txs-response.ts
@@ -28,7 +28,8 @@ export class QueryBlockTxsResponse {
 
       return new QueryBlockTxsResponse(
         transactions,
-        rawObjValue.total_count
+        rawObjValue.total_txs,
+        rawObjValue.page_count
       )
     } catch (error) {
       throw error
@@ -37,6 +38,7 @@ export class QueryBlockTxsResponse {
 
   public readonly transactions: Transaction[]
   public readonly totalCount: number
+  public readonly pageCount: number
 
   /**
    * Query block transactions Response.
@@ -46,10 +48,12 @@ export class QueryBlockTxsResponse {
    */
   constructor(
     transactions: Transaction[],
-    totalCount: number
+    totalCount: number,
+    pageCount: number
   ) {
     this.transactions = transactions
     this.totalCount = totalCount
+    this.pageCount = pageCount
 
     if (!this.isValid()) {
       throw new TypeError("Invalid QueryBlockTxsResponse properties.")
@@ -71,7 +75,8 @@ export class QueryBlockTxsResponse {
 
     return {
       txs: transactions,
-      total_count: this.totalCount
+      total_count: this.totalCount,
+      page_count: this.pageCount
     }
   }
   /**
